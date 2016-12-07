9f77cad8670ebc93a28653da198aeb9049ebd3c9
henices <zhouzhenster@gmail.com>
2016-12-07 03:55:12 -0600

    Update google-chrome-font.user.js
    make fonts looks better

--- google-chrome-font.user.js
+++ google-chrome-font.user.js
@@ -4,9 +4,8 @@
 // @description  Better font on Google Chrome browser
 // @match        http://*/*
 // @match        https://*/*
-// @exclude      *github.com*
 // @grant        GM_addStyle
 // @copyright    @henices
 // ==/UserScript==
 
-GM_addStyle('body {font-family:  "WenQuanYi Micro Hei", "Microsoft Yahei", "DejaVu Serif" "DejaVu Sans" !important; }');
+GM_addStyle('body {font-family: "Helvetica Neue",Helvetica,"Microsoft YaHei",Arial,sans-serif !important; font-size: 14px !important;}');
